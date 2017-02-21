04fa746cc84746ec0e5d63ca5a445a7b13d24a6c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-02-21 22:03:13 +0000

    Updates

--- Core/Source/TTL.cpp
+++ Core/Source/TTL.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "TTL.h"
@@ -30,19 +30,19 @@ namespace DNS
 {
 
 TTL::TTL(uint32_t ttl)
-	: m_TTL(ttl)
+    : m_TTL(ttl)
 {
 }
 
 TTL::TTL(const std::string& ttl)
-	: m_TTL(0), m_TTLString(ttl)
+    : m_TTL(0), m_TTLString(ttl)
 {
 }
 
 void TTL::writeBinary(std::ostream& stream) const
 {
-	uint32_t tmp2 = boost::endian::native_to_big(m_TTL);
-	stream.write((char*)&tmp2, 4);
+    uint32_t tmp2 = boost::endian::native_to_big(m_TTL);
+    stream.write((char*)&tmp2, 4);
 }
 
 void TTL::writeText(std::ostream& stream) const
--- Include/Ishiko/DNS/Core/TTL.h
+++ Include/Ishiko/DNS/Core/TTL.h
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #ifndef _ISHIKO_DNS_CORE_TTL_H_
@@ -38,18 +38,18 @@ namespace DNS
 class TTL
 {
 public:
-	TTL(uint32_t ttl);
-	TTL(const std::string& ttl);
+    TTL(uint32_t ttl);
+    TTL(const std::string& ttl);
 
-	void writeBinary(std::ostream& stream) const;
-	void writeText(std::ostream& stream) const;
+    void writeBinary(std::ostream& stream) const;
+    void writeText(std::ostream& stream) const;
 
 private:
-	// The TTL expressed in seconds
-	uint32_t m_TTL;
-	// If the TTL was constructed by specifiying the TTL using a string
-	// like 1, or 2w, then the m_TTLString will be set to that value
-	std::string m_TTLString;
+    // The TTL expressed in seconds
+    uint32_t m_TTL;
+    // If the TTL was constructed by specifiying the TTL using a string
+    // like 1, or 2w, then the m_TTLString will be set to that value
+    std::string m_TTLString;
 };
 
 }
