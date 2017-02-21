22484e605ee206fb111f7524d37faaabed925ac5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-02-21 10:18:57 +0000

    Replaced tabs with spaces

--- Tests/Core/Source/AddressRecordTests.cpp
+++ Tests/Core/Source/AddressRecordTests.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "AddressRecordTests.h"
@@ -26,28 +26,28 @@
 
 void AddAddressRecordTests(TestHarness& theTestHarness)
 {
-	TestSequence& addressRecordTestSequence = theTestHarness.appendTestSequence("AddressRecord tests");
+    TestSequence& addressRecordTestSequence = theTestHarness.appendTestSequence("AddressRecord tests");
 
-	new HeapAllocationErrorsTest("Creation test 1", AddressRecordCreationTest1, addressRecordTestSequence);
-	new FileComparisonTest("write test 1", AddressRecordWriteBinaryTest1, addressRecordTestSequence);
+    new HeapAllocationErrorsTest("Creation test 1", AddressRecordCreationTest1, addressRecordTestSequence);
+    new FileComparisonTest("write test 1", AddressRecordWriteBinaryTest1, addressRecordTestSequence);
 }
 
 TestResult::EOutcome AddressRecordCreationTest1()
 {
-	Ishiko::DNS::AddressRecord addressRecord("example.org.", 86400);
-	return TestResult::ePassed;
+    Ishiko::DNS::AddressRecord addressRecord("example.org.", 86400);
+    return TestResult::ePassed;
 }
 
 TestResult::EOutcome AddressRecordWriteBinaryTest1(FileComparisonTest& test)
 {
-	boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "NameServerRecordWriteBinaryTest1.bin");
-	std::ofstream stream(outputPath.c_str());
+    boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "NameServerRecordWriteBinaryTest1.bin");
+    std::ofstream stream(outputPath.c_str());
 
-	Ishiko::DNS::AddressRecord addressRecord("example.org.", 86400);
-	addressRecord.writeBinary(stream);
+    Ishiko::DNS::AddressRecord addressRecord("example.org.", 86400);
+    addressRecord.writeBinary(stream);
 
-	test.setOutputFilePath(outputPath);
-	test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "NameServerRecordWriteBinaryTest1.bin");
+    test.setOutputFilePath(outputPath);
+    test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "NameServerRecordWriteBinaryTest1.bin");
 
-	return TestResult::ePassed;
+    return TestResult::ePassed;
 }
