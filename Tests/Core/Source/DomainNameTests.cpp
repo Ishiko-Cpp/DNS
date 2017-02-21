41ab0001f1ef969addea0742026839803e51103b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-02-21 10:24:27 +0000

    Updates

--- Tests/Core/Source/DomainNameTests.cpp
+++ Tests/Core/Source/DomainNameTests.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2010-2015 Xavier Leclercq
+    Copyright (c) 2010-2017 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "DomainNameTests.h"
@@ -26,60 +26,65 @@
 
 void AddDomainNameTests(TestHarness& theTestHarness)
 {
-	TestSequence& domainNameTestSequence = theTestHarness.appendTestSequence("DomainName tests");
+    TestSequence& domainNameTestSequence = theTestHarness.appendTestSequence("DomainName tests");
 
-	new HeapAllocationErrorsTest("Creation test 1", DomainNameCreationTest1, domainNameTestSequence);
-	new HeapAllocationErrorsTest("Creation test 2", DomainNameCreationTest2, domainNameTestSequence);
-	new FileComparisonTest("write test 1", DomainNameWriteTest1, domainNameTestSequence);
-	new FileComparisonTest("write test 2", DomainNameWriteTest2, domainNameTestSequence);
+    new HeapAllocationErrorsTest("Creation test 1", DomainNameCreationTest1, domainNameTestSequence);
+
+    new HeapAllocationErrorsTest("initializeFromStream test 1", DomainNameInitializeFromStreamTest1, domainNameTestSequence);
+
+    new FileComparisonTest("write test 1", DomainNameWriteTest1, domainNameTestSequence);
+    new FileComparisonTest("write test 2", DomainNameWriteTest2, domainNameTestSequence);
 }
 
 TestResult::EOutcome DomainNameCreationTest1()
 {
-	Ishiko::DNS::DomainName domainName("www.dummy.com");
-	return TestResult::ePassed;
+    Ishiko::DNS::DomainName domainName("www.dummy.com");
+    return TestResult::ePassed;
 }
 
-TestResult::EOutcome DomainNameCreationTest2(Test& test)
+TestResult::EOutcome DomainNameInitializeFromStreamTest1(Test& test)
 {
-	TestResult::EOutcome result = TestResult::eFailed;
+    TestResult::EOutcome result = TestResult::eFailed;
 
-	boost::filesystem::path inputPath(test.environment().getTestDataDirectory() / "DomainNameCreationTest2.bin");
-	std::ifstream stream(inputPath.c_str());
+    boost::filesystem::path inputPath(test.environment().getTestDataDirectory() / "DomainNameCreationTest2.bin");
+    std::ifstream stream(inputPath.c_str());
 
-	Ishiko::DNS::DomainName domainName(stream);
-	if (domainName == "www.dummy.com.")
-	{
-		result = TestResult::ePassed;
-	}
-	
-	return result;
+    Ishiko::DNS::DomainName domainName;
+    if (domainName.initializeFromStream(stream).succeeded())
+    {
+        if (domainName == "www.dummy.com.")
+        {
+            result = TestResult::ePassed;
+        }
+    }
+    
+    return result;
 }
 
 TestResult::EOutcome DomainNameWriteTest1(FileComparisonTest& test)
 {
-	boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "DomainNameWriteTest1.bin");
-	std::ofstream stream(outputPath.c_str());
+    boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "DomainNameWriteTest1.bin");
+    std::ofstream stream(outputPath.c_str());
 
-	Ishiko::DNS::DomainName domainName("www.dummy.com");
-	domainName.write(stream);
+    Ishiko::DNS::DomainName domainName("www.dummy.com");
+    domainName.write(stream);
 
-	test.setOutputFilePath(outputPath);
-	test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "DomainNameWriteTest1.bin");
+    test.setOutputFilePath(outputPath);
+    test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "DomainNameWriteTest1.bin");
 
-	return TestResult::ePassed;
+    return TestResult::ePassed;
 }
 
 TestResult::EOutcome DomainNameWriteTest2(FileComparisonTest& test)
 {
-	boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "DomainNameWriteTest2.bin");
-	std::ofstream stream(outputPath.c_str());
+    boost::filesystem::path outputPath(test.environment().getTestOutputDirectory() / "DomainNameWriteTest2.bin");
+    std::ofstream stream(outputPath.c_str());
 
-	Ishiko::DNS::DomainName domainName("www.dummy.com.");
-	domainName.write(stream);
+    Ishiko::DNS::DomainName domainName("www.dummy.com.");
+    domainName.write(stream);
 
-	test.setOutputFilePath(outputPath);
-	test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "DomainNameWriteTest2.bin");
+    test.setOutputFilePath(outputPath);
+    test.setReferenceFilePath(test.environment().getReferenceDataDirectory() / "DomainNameWriteTest2.bin");
 
-	return TestResult::ePassed;
+    return TestResult::ePassed;
 }
